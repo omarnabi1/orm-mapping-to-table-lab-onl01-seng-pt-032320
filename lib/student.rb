b0ac17426bcc71aa1d88de30fee9f49f79d5940f
Omar Nabi <onabi20@gmail.com>
2020-05-27 02:47:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
  attr_accessor :name, :grade
  attr_reader :id
   
-  def initialize (name, grade, id=nill)
+  def initialize (name, grade, id = nill)
     @name = name
     @grade = grade
     @id = id 
@@ -11,7 +11,7 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXIST songs (
+      CREATE TABLE IF NOT EXIST students (
       id INTEGER PRIMARY KEY
       name TEXT, 
       album TEXT
