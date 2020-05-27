79a2ab79e2f9f79336707623329cce71bf39a208
Omar Nabi <onabi20@gmail.com>
2020-05-27 02:58:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,9 @@ class Student
           DB[:conn].execute(sql)
   end
   
-  
+  def save
+    sql = <<-SQL
+          INSERT INTO students (name, grade)
+          VALUES (?, ?)
             
 end
